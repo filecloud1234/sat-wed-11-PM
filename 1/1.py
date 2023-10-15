f9f3253c6ea02b66cee0eef797e45ab2ed34f9ff
filecloud1234 <110065019+filecloud1234@users.noreply.github.com>
2023-10-15 02:31:48 -0700

    Add files via upload

---
+++ 1/1.py
@@ -0,0 +1,29 @@
+# input("Enter Your Number:")
+# print("Hi")
+
+# test = 1234 # Integer
+# test = 1234.15 # Float
+# test = "hello1234*#$%" # String
+# test = True # False, 0, 1 # Boolean
+
+# int()
+# float()
+# str()
+# bool()
+
+# print(type(test))
+
+
+# test1 = 2
+# test2 = "b"
+# test3 = 1
+
+# print(test1 + test3)
+# print(test1 - test3)
+# print(test1 * test3)
+# print(test1 / test3)
+
+# print(str(test1) + test2)
+# print(test1 * test2)
+
+# print(type(input("Enter Your Number:")))
---
+++ 1/2.py
@@ -0,0 +1,4 @@
+x = input("Enter Your Number:")
+y = int(x) + 3
+print(y)
+print(int(x) + 3)
